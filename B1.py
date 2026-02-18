d84190d7d3a015b9b975d7f60cb4a7ee33e74928
AndresFCastro <andresfe.castrote@ecci.edu.co>
2026-02-18 09:18:52 -0500

    Restructuracion

--- B1.py
+++
@@ -1,8 +0,0 @@
-print("=== CALCULO DE POTENCIA ELECTRICA ===")
-
-voltaje = float(input("Ingrese el valor del voltaje (V): "))
-corriente = float(input("Ingrese el valor de la corriente (A): "))
-
-potencia = voltaje * corriente
-
-print(f"La potencia consumida es: {potencia:.2f} Watts")
--- B5.py
+++
@@ -1,6 +0,0 @@
-respuesta = ""
-
-while respuesta.lower() != "no":
-    respuesta = input("¿Desea continuar? (Si/No): ")
-
-print("Programa finalizado.")
